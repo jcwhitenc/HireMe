a66f5a4b87808605b9168a3de3d7623c112d5b07
JC White <jcwhite.nc@gmail.com>
2022-03-30 12:39:22 -0600

    Basic Functionality, setup main activity

--- app/src/main/java/com/cs356/hireme/MainActivity.kt
+++ app/src/main/java/com/cs356/hireme/MainActivity.kt
@@ -2,8 +2,8 @@ package com.cs356.hireme
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.LinearLayout
+import android.widget.ImageButton
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomsheet.BottomSheetDialog
@@ -18,11 +18,42 @@ class MainActivity : AppCompatActivity() {
 
         //checkCurrentUser()
 
-        val mBottton = findViewById<Button>(R.id.button)
-        mBottton.setOnClickListener {
+        // Image clicked
+        val companyImage = findViewById<ImageButton>(R.id.company_image)
+        companyImage.setOnClickListener {
             showBottomSheetDialog()
         }
 
+        // Set up the profile button
+        val profileButton = findViewById<ImageButton>(R.id.profile_button)
+        profileButton.setOnClickListener{
+            // Put up the Profile Fragments
+            Toast.makeText(this, "Show Profile", Toast.LENGTH_SHORT).show()
+        }
+
+        // accept button
+        val acceptButton = findViewById<TextView>(R.id.accept_button)
+        acceptButton.setOnClickListener {
+            acceptCompany()
+        }
+
+        // reject button
+        val rejectButton = findViewById<TextView>(R.id.reject_button)
+        rejectButton.setOnClickListener {
+            rejectCompany()
+        }
+    }
+
+    private fun acceptCompany() {
+        getNewCompany()
+    }
+
+    private fun getNewCompany() {
+        Toast.makeText(this,"Getting new Company", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun rejectCompany() {
+        getNewCompany()
     }
 
     private fun showBottomSheetDialog() {
